b114ad21a67e83e3c36731929075c48d6fae6503
ThanhQuy99 <quy.tran@enotion.io>
2024-03-20 17:15:21 +0700

    update import data

--- Scripts/MultiSlotConsole.js
+++ Scripts/MultiSlotConsole.js
@@ -10,18 +10,50 @@ cc.Class({
 
     onLoad() {
         this.poolFactory = this.node.poolFactory;
-        this.configGame=this.configGameConsole.json;
     },
 
     addNewGame() {
         const gameId = this.editBox.string;
+        const configGame = this.configGameConsole.json;
         if (gameId && gameId.length) {
-            const config = this.configGame && this.configGame[gameId] ? this.configGame[gameId] : this.configGame.all;
+            const config = configGame && configGame[gameId] ? configGame[gameId] : configGame.all;
             const newGamePrefab = this.poolFactory.getObject("ConsoleGame");
             newGamePrefab.gameId = gameId;
             newGamePrefab.configGameConsole = config;
             newGamePrefab.parent = this.layoutHolder;
             newGamePrefab.active = true;
+        }
+    },
+    exportFile() {
+        this._exportDataFile(JSON.stringify(this.configGameConsole), "ConfigGameConsole");
+    },
+    importFile() {
+        this._importDataFile((data) => {
+            this.configGameConsole = JSON.parse(data);
+        });
+    },
+
+
+    _exportDataFile(dataStr, fileName) {
+        let type = fileName.split(".").pop();
+        const dataUri = `data:application/${type};charset=utf-8,${encodeURIComponent(dataStr)}`;
+        const linkElement = document.createElement('a');
+        linkElement.setAttribute('href', dataUri);
+        linkElement.setAttribute('download', fileName);
+        linkElement.click();
+    },
+    _importDataFile(callback) {
+        const input = window.document.createElement("input");
+        input.type = "file";
+        setTimeout(() => { input.click(); }, 500);
+        input.onchange = () => {
+            const selectedFile = input.files[0];
+            const reader = new FileReader();
+            reader.onload = (event) => {
+                console.log("file loaded", event.target.result);
+                callback(event.target.result);
+            };
+            reader.readAsText(selectedFile);
         }
     }
 });
